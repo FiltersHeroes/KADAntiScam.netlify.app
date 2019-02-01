96a5d5ca8aa56c5da5c078ac096f994fbb2d0235
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-01 22:17:09 +0100

    Odświeżenie wyglądu
    Zupełnie nowy wygląd, ciemny motyw Meghna

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+jQuery(function ($) {
+	"use strict";
+
+	/* ========================================================================= */
+	/*	Page Preloader
+	/* ========================================================================= */
+
+	// Preloader js
+	$(window).on('load', function () {
+		$('#preloader').fadeOut(700);
+	});
+
+	/* ========================================================================= */
+	/*	Menu item highlighting
+	/* ========================================================================= */
+
+
+	$("#navigation").sticky({
+		topSpacing: 0
+	});
+
+});
+// End Jquery Function
+
+
+/* ========================================================================= */
+/*	Animated section
+/* ========================================================================= */
+
+var wow = new WOW({
+	offset: 100, // distance to the element when triggering the animation (default is 0)
+	mobile: false // trigger animations on mobile devices (default is true)
+});
+wow.init();
+
+
+/* ========================================================================= */
+/*	Smooth Scroll
+/* ========================================================================= */
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
---
+++ content/blog/_index.md
@@ -0,0 +1,3 @@
+---
+title: "Blog polskiego filtra blokującego strony typu przekręt | KAD - stop przekrętom"
+---
