cfddefbf8a66a751643f711398de43390699ab43
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-18 18:12:57 +0100

    Aktualizacja Bootstrapa i inne ulepszenia

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,30 +32,30 @@ function init() {
 		};
 
 // EasyMDE - Markdown editor
-var easyMDE = new EasyMDE({
-	autoDownloadFontAwesome: false,
-	spellChecker: false,
-	styleSelectedText: false,
-	hideIcons: ["fullscreen", "side-by-side", "heading"],
-	status: false,
-	showIcons: ["code", "strikethrough"]
-});
+// var easyMDE = new EasyMDE({
+// 	autoDownloadFontAwesome: false,
+// 	spellChecker: false,
+// 	styleSelectedText: false,
+// 	hideIcons: ["fullscreen", "side-by-side", "heading"],
+// 	status: false,
+// 	showIcons: ["code", "strikethrough"]
+// });
 
 // Polish translation of EasyMDE
-if(document.getElementsByTagName('html')[0].getAttribute('lang')=="pl-PL")
-{
-	if (document.querySelector('.editor-toolbar')){
-		document.querySelector(".editor-toolbar button.bold").setAttribute("title", "Pogrubienie (Ctrl-B)");
-		document.querySelector(".editor-toolbar button.italic").setAttribute("title", "Kursywa (Ctrl-I)");
-		document.querySelector(".editor-toolbar button.strikethrough").setAttribute("title", "Przekreślenie");
-		document.querySelector(".editor-toolbar button.code").setAttribute("title", "Kod (Ctrl-Alt-C)");
-		document.querySelector(".editor-toolbar button.quote").setAttribute("title", "Cytat (Ctrl-')");
-		document.querySelector(".editor-toolbar button.unordered-list").setAttribute("title", "Lista wypunktowana (Ctrl-L)");
-		document.querySelector(".editor-toolbar button.ordered-list").setAttribute("title", "Lista numerowana (Ctrl-Alt-L)");
-		document.querySelector(".editor-toolbar button.link").setAttribute("title", "Utwórz link (Ctrl-K)");
-		document.querySelector(".editor-toolbar button.image").setAttribute("title", "Wstaw obraz (Ctrl-Alt-I)");
-		document.querySelector(".editor-toolbar button.preview").setAttribute("title", "Podgląd (Ctrl-P)");
-		document.querySelector(".editor-toolbar button.guide").setAttribute("title", "Poradnik Markdown");
-	}
-}
+// if(document.getElementsByTagName('html')[0].getAttribute('lang')=="pl-PL")
+// {
+// 	if (document.querySelector('.editor-toolbar')){
+// 		document.querySelector(".editor-toolbar button.bold").setAttribute("title", "Pogrubienie (Ctrl-B)");
+// 		document.querySelector(".editor-toolbar button.italic").setAttribute("title", "Kursywa (Ctrl-I)");
+// 		document.querySelector(".editor-toolbar button.strikethrough").setAttribute("title", "Przekreślenie");
+// 		document.querySelector(".editor-toolbar button.code").setAttribute("title", "Kod (Ctrl-Alt-C)");
+// 		document.querySelector(".editor-toolbar button.quote").setAttribute("title", "Cytat (Ctrl-')");
+// 		document.querySelector(".editor-toolbar button.unordered-list").setAttribute("title", "Lista wypunktowana (Ctrl-L)");
+// 		document.querySelector(".editor-toolbar button.ordered-list").setAttribute("title", "Lista numerowana (Ctrl-Alt-L)");
+// 		document.querySelector(".editor-toolbar button.link").setAttribute("title", "Utwórz link (Ctrl-K)");
+// 		document.querySelector(".editor-toolbar button.image").setAttribute("title", "Wstaw obraz (Ctrl-Alt-I)");
+// 		document.querySelector(".editor-toolbar button.preview").setAttribute("title", "Podgląd (Ctrl-P)");
+// 		document.querySelector(".editor-toolbar button.guide").setAttribute("title", "Poradnik Markdown");
+// 	}
+// }
 
