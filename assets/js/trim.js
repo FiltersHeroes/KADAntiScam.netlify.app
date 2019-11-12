bd78d8c64237d05751d350d370ea3e0819aebf5d
hawkeye116477 <hawkeye116477@gmail.com>
2019-11-12 14:16:49 +0100

    Opis oszukańczej praktyki obowiązkowy 😊

--- assets/js/trim.js
+++ assets/js/trim.js
@@ -7,4 +7,13 @@ $(function() {
             $(this).val(value.replace(reg, value));
         }
     })
+    $('#additional-info').one("keyup", function(event) {
+        var tavalue = $(this).val();
+        var tareg = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+        if (tareg.test(tavalue)) {
+            document.querySelector("#direct-link").value = document.querySelector("#additional-info").value;
+            $(this).val(tavalue.replace(tareg, ""));
+            alert("Nie tak szybko kowboju, sam link nie wystarczy, musisz jeszcze podać opis oszustwa!");
+        }
+    })
 });
