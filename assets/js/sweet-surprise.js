74733e37b5ab2ddc676fa70867eaf5c27f157196
hawkeye116477 <hawkeye116477@gmail.com>
2020-01-03 19:01:40 +0100

    Prosimy wysyłać zgłoszenia nie częściej niż co 3 minuty 😊

--- assets/js/sweet-surprise.js
+++ assets/js/sweet-surprise.js
@@ -4,5 +4,18 @@ $(document).ready(function() {
             $('form').attr('action', '/');
             return false;
         }
+        else if ("submittedTime" in localStorage &&
+        new Date().getHours() == new Date(localStorage.getItem("submittedTime")).getHours() &&
+        new Date().getDate() == new Date(localStorage.getItem("submittedTime")).getDate() &&
+        new Date().getDate() == new Date(localStorage.getItem("submittedTime")).getMonth() &&
+        new Date().getFullYear() == new Date(localStorage.getItem("submittedTime")).getFullYear() &&
+        new Date().getMinutes() - new Date(localStorage.getItem("submittedTime")).getMinutes() < 3) {
+                $('form').attr('action', '/');
+                Swal.fire("Zbyt często wysyłasz zgłoszenia!", "Musisz odczekać co najmniej 3 minuty przed wysłaniem kolejnego zgłoszenia 😞", "error");
+                return false;
+        }
+        else {
+            Swal.fire("Zgłoszenie zostało wysłane na GitHuba!", "Dziękujemy za wypełnienie formularza 😊", "success");
+        }
     });
 });
