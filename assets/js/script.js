25ce27481981d594dee3db1094a2c9558bb53cd1
hawkeye116477 <hawkeye116477@gmail.com>
2020-05-10 23:22:57 +0200

    Poprawka

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,9 +54,9 @@ function checkAdblocker(){
 	else if(kad.offsetHeight == 0)
 	{
 		Swal.fire({
-			title: 'Dobrze, choć mogłoby być lepiej!',
+			title: 'Źle!',
 			icon: 'warning',
-			html: 'Prawdopodobnie masz aktywny AdBlock/AdBlock Plus z listą KAD. Zalecamy zamienić go na <a href="https://github.com/gorhill/uBlock#installation" target="_blank" rel="noopener">uBlock Origin</a>, by mieć lepszą ochronę przed przekrętami.'
+			html: 'Prawdopodobnie masz aktywny AdBlock/AdBlock Plus z listą KAD. Do poprawnego działania listy wymagane jest rozszerzenie <a href="https://github.com/gorhill/uBlock#installation" target="_blank" rel="noopener">uBlock Origin</a>, <a href="https://github.com/NanoAdblocker/NanoCore#install-links" target="_blank" rel="noopener">Nano Adblocker</a> lub <a href="https://github.com/AdguardTeam/AdguardBrowserExtension#installation" target="_blank" rel="noopener">AdGuard</a>.'
 		  })
 	}
 	else
