5c596076418d1c0d6f827782189d4173d7f8eb96
hawkeye116477 <hawkeye116477@gmail.com>
2021-08-12 12:23:48 +0200

    Kolejne niespodzianki
    Czyli bez JS nie przejdą...

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     autosize($('textarea'));
-    $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSdHY8a2G3f9hLv-gar86DYdlf6gsnI6E10Vlh2x-IQpkOE25A/formResponse');
+    if($('#usrform')) {
+        $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSdHY8a2G3f9hLv-gar86DYdlf6gsnI6E10Vlh2x-IQpkOE25A/formResponse');
+    }
+    if ($('#commentsform')) {
+        $('#commentsform').attr('action', $('#commentsform').data("action"));
+        $('#commentsform').removeAttr("data-action");
+    }
 });
